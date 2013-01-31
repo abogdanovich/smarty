c11f746d9c4c6db793266c49e61520f0524eb7c1
Alex Bogdanovich <alex@abogdanovich-laptop.(none)>
2013-01-31 16:40:28 +0300

    base smarty classes

---
+++ web/sensors.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+# Smarty home system sensors
+# author Alex Bogdanovich
+# 2013 
+#########################################################################
+
+#########################################################################
+#Sensors class 
+#########################################################################
+
+class Sensor:
+    def __init__(self, uid, alias, type, status, value, date, access_time):
+        self.uid = uid
+        self.alias = alias
+        self.type = type
+        self.status = status
+        self.value = value
+        self.access_time = access_time
+        return self.uid
+
